03034a42e2ada89681195c54ec60edbc7151388f
tibz <shaveddoor@gmail.com>
2023-10-19 17:30:02 -0400

    initial commit

---
+++ lab-5/.js
@@ -0,0 +1,21 @@
+const firstName = "Mohammad";
+const lastName = "Elkhatib";
+const fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+let population = 100000000;
+console.log(population);
+
+population = population * 3;
+console.log(population);
+
+let javaScriptIsCool = true;
+const colors = ["red", "yellow", "orange", "green", "blue", "purple"];
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
