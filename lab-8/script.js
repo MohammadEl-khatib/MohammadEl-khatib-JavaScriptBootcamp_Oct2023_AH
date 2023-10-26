c7d3788a1260da7b263af43db2f685caaee54856
tibz <shaveddoor@gmail.com>
2023-10-26 04:34:57 -0400

    lab-8

---
+++ lab-8/script.js
@@ -0,0 +1,30 @@
+for(let i =1;i<=10; i++) {
+    console.log(i)
+}
+for(let i =10;i>=1; i--) {
+    console.log(i)
+}
+var i=1;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+let x=10;
+while(x>=1){
+    console.log(x);
+    x--;
+}
+let y=1;
+do{
+console.log(y);
+y++;
+} while(y<=10);
+let z=10;
+do{
+console.log(z);
+z--;
+} while(z>=1);
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (const p of numbers) {
+    console.log(p)
+}
